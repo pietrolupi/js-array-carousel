c560b191b00bf04d16ed1b867848087ca4b987e5
pietrolupi <pietro.lupi92@gmail.com>
2023-09-16 14:24:33 +0200

    [edit]: js, optimized with functions

--- JS/script.js
+++ JS/script.js
@@ -15,50 +15,33 @@ const btnBot = document.querySelector('.bot');
 
 let counterImg = 0;
 
-for(let i = 0; i < images.length; i++){
-  const image = images[i];
-  
-  picturesContainer.innerHTML += `<img src="${image}" class="item d-none" alt="">`;
-};
+listPrinter(picturesContainer, images);
 
 const itemsCollection = document.getElementsByClassName('item');
 const maskCollection = document.getElementsByClassName('mask');
 const smallImgCollection = document.getElementsByClassName('img-small');
 
-itemsCollection[counterImg].classList.remove('d-none');
-maskCollection[counterImg].classList.add('d-none');
-smallImgCollection[counterImg].classList.add('selected');
+showNext();
 
 //BOTTONE BOTTOM/////////////////////////////////////////////
 btnBot.addEventListener('click', function(){
   
   if(counterImg === images.length - 1){
   
-  itemsCollection[counterImg].classList.add('d-none');
-  smallImgCollection[counterImg].classList.remove('selected');
-  maskCollection[counterImg].classList.remove('d-none');
+  hidePrev();
   
   counterImg = 0;
   
-  itemsCollection[counterImg].classList.remove('d-none');
-  smallImgCollection[counterImg].classList.add('selected');
-  maskCollection[counterImg].classList.add('d-none');
-    
+  showNext();    
     
   }else{
-    btnTop.classList.remove('d-none');
-
-  itemsCollection[counterImg].classList.add('d-none');
-  smallImgCollection[counterImg].classList.remove('selected');
-  maskCollection[counterImg].classList.remove('d-none');
   
-
+  hidePrev(); 
+  
   counterImg ++;
   
-  itemsCollection[counterImg].classList.remove('d-none');
-  smallImgCollection[counterImg].classList.add('selected');
-  maskCollection[counterImg].classList.add('d-none');
-  console.log(counterImg)
+  showNext();  
+
   }
 });
 
@@ -69,33 +52,76 @@ btnBot.addEventListener('click', function(){
 btnTop.addEventListener('click', function(){
   
   if(counterImg === 0){
-
-  itemsCollection[counterImg].classList.add('d-none');
-  smallImgCollection[counterImg].classList.remove('selected');
-  maskCollection[counterImg].classList.remove('d-none');
+  
+  hidePrev(); 
 
   counterImg = 4;
-
-  itemsCollection[counterImg].classList.remove('d-none');
-  smallImgCollection[counterImg].classList.add('selected');
-  maskCollection[counterImg].classList.add('d-none');
+  
+  showNext();  
     
   }else{
   
   btnBot.classList.remove('d-none');
   
-  itemsCollection[counterImg].classList.add('d-none');
-  smallImgCollection[counterImg].classList.remove('selected');
-  maskCollection[counterImg].classList.remove('d-none');
-
+  hidePrev(); 
+  
   counterImg--;
 
-  itemsCollection[counterImg].classList.remove('d-none');
-  smallImgCollection[counterImg].classList.add('selected');
-  maskCollection[counterImg].classList.add('d-none');
+  showNext(); 
+ 
   }
 });
 
-console.log(itemsCollection[0]);
-console.log(itemsCollection[4])
-console.log(counterImg)
+
+//FUNZIONI////////
+
+
+/**
+ * Mostra/nasconde o evidenzia/disevidenzia un elemento con il comando apposito.
+ * @param {elementToShow} what 
+ * @param {show/hide/active/inactive} show 
+ */
+function toggler(what, show){
+
+  let pippo = show;
+  
+  if(pippo === 'show'){
+    what.classList.remove('d-none');
+  }else if(pippo === 'hide'){
+    what.classList.add('d-none');
+  }else if(pippo === 'active'){
+    what.classList.add('selected')
+  }else if(pippo === 'inactive'){
+    what.classList.remove('selected')
+  }else{
+    console.log('ERROR ERROR insert show or hide')
+  };
+
+}
+
+/**
+ * stampa tutti gli elementi di una lista all'interno di un div
+ * @param {whereToPrintInside} whereToPrint 
+ * @param {nameOfTheListToPrint} arrayName 
+ */
+function listPrinter(whereToPrint, arrayName){
+  
+  for(let c = 0; c < arrayName.length; c++){
+    whereToPrint.innerHTML += `<img src="${arrayName[c]}" class="item d-none" alt="">`
+    console.log(c)
+  }
+
+}
+
+
+function hidePrev(){
+  toggler(itemsCollection[counterImg], 'hide');
+  toggler(smallImgCollection[counterImg], 'inactive');
+  toggler(maskCollection[counterImg], 'show');
+}
+
+function showNext(){
+  toggler(itemsCollection[counterImg], 'show');
+  toggler(smallImgCollection[counterImg], 'active');
+  toggler(maskCollection[counterImg], 'hide'); 
+}
